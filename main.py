6ca1d55a76da2cd46fd6af4147594e846d60ea0e
Leporoni <leporoni@hotmail.com>
2024-08-28 10:55:49 -0300

    adjust code to search .pdf files in pdf_directory

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import os
+
 from pdf_tools.PDFReader import PDFReader
 from pdf_tools.KeywordClassifier import KeywordClassifier
 from pdf_tools.PDFKeywordProcessor import PDFKeywordProcessor
 
 if __name__ == "__main__":
-    file_path = "curriculums/JohnDoeResume.pdf"
     keyword = "palavra-chave"
+    pdf_directory = "curriculums"
 
     pdf_reader = PDFReader()
     keyword_classifier = KeywordClassifier()
     processor = PDFKeywordProcessor(pdf_reader, keyword_classifier)
 
-    result = processor.process(file_path, keyword)
-    print(f"Palavra-chave encontrada? {result}")
+    for filename in os.listdir(pdf_directory):
+        if filename.endswith(".pdf"):
+            file_path = os.path.join(pdf_directory, filename)
+            result = processor.process(file_path, keyword)
+            print(f"Palavra-chave encontrada no arquivo {filename}? {result}")
